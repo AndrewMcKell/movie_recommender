0942498ed291fc98d85571a5653b885c7e100b8e
AndrewMcKell <amckell33@icloud.com>
2026-09-12 18:12:54 -0400

    First test of api

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+def main():
+    url = "https://api.themoviedb.org/3/search/movie"
+    token = os.getenv("TMDB_TOKEN")
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {token}"
+        }
+    movie_title = "City of God"
+    params = {
+        "query": movie_title
+    }
+    response = requests.get(url, headers=headers, params=params)
+    print(response.text)
+    
+
+if __name__ == "__main__":
+    main()
