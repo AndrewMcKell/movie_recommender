a506a0ec3094fba8f0aed7b086b57cd8d011987a
AndrewMcKell <amckell33@icloud.com>
2026-09-19 15:55:18 -0400

    Finds and stores complete movie details including keywords, first ten cast members and director

--- main.py
+++ main.py
@@ -3,14 +3,30 @@ import json
 import requests
 from dotenv import load_dotenv
 
-from search_movies import search_movie_by_title
+from search_movies import (
+    search_movie_by_title,
+    get_movie_details,
+    get_movie_keywords,
+    get_movie_credits,
+    get_director
+)
 
 
 def main():
-    movie_data = search_movie_by_title("City of God")
-    with open("result.json", "w") as file:
-        json.dump(movie_data, file, indent=4)
-    
+    movie_search = search_movie_by_title("City of God")
+    movie_id = movie_search["id"]
+    movie_data = get_movie_details(movie_id)
+    keyword_data = get_movie_keywords(movie_id)
+    credit_data = get_movie_credits(movie_id)
+    result = {}
+    result["details"] = movie_data
+    result["keywords"] = keyword_data
+    result["cast"] = credit_data["cast"][:10]
+    director_data = get_director(credit_data)
+    result["director"] = director_data
+    with open("result.json", "w") as input_movie:
+        json.dump(result, input_movie, indent=4)
+
 
 if __name__ == "__main__":
     main()
--- search_movies.py
+++ search_movies.py
@@ -1,5 +1,4 @@
 import os
-import json
 import requests
 from dotenv import load_dotenv
 
@@ -18,3 +17,45 @@ def search_movie_by_title(title: str):
     response_data = response.json()
     top_result_data = response_data["results"][0]
     return top_result_data
+
+def get_movie_details(id: int):
+    load_dotenv()
+    url = f"https://api.themoviedb.org/3/movie/{id}"
+    token = os.getenv("TMDB_TOKEN")
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {token}"
+        }
+    response = requests.get(url, headers=headers)
+    result_data = response.json()
+    return result_data
+
+def get_movie_keywords(id: int):
+    load_dotenv()
+    url = f"https://api.themoviedb.org/3/movie/{id}/keywords"
+    token = os.getenv("TMDB_TOKEN")
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {token}"
+        }
+    response = requests.get(url, headers=headers)
+    keyword_data = response.json()
+    return keyword_data
+
+def get_movie_credits(id: int):
+    load_dotenv()
+    url = f"https://api.themoviedb.org/3/movie/{id}/credits"
+    token = os.getenv("TMDB_TOKEN")
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {token}"
+        }
+    response = requests.get(url, headers=headers)
+    credit_data = response.json()
+    return credit_data
+
+def get_director(credits: dict):
+    for person in credits["crew"]:
+        if person["job"] == "Director":
+            director_details = person
+    return director_details
